fefb7fff332e2b24c6d266007cb2705ab531c0f0
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-05-18 21:54:11 -0700

    added event listener to form file to store users post in local storage

---
+++ assets/js/form.js
@@ -0,0 +1,46 @@
+
+
+// Array to hold each post and credential 
+let userPostInfo = [];
+
+
+
+const userName= document.querySelector("#user-login");
+
+const titelOfPost = document.querySelector("#title-post");
+
+const usersPost = document.querySelector("#msg");
+
+
+const submitButton = document.querySelector("#post");
+
+// This function stores newly added post to the local storage
+function storePost(){
+
+localStorage.setItem("userPostInfo", JSON.stringify(userPostInfo));
+
+}
+
+// This submit all the data
+submitButton.addEventListener("click", function(event){
+
+event.preventDefault();
+
+let userInput = userName.value;
+
+let userTitle = titelOfPost.value;
+
+let content = usersPost.value;
+
+console.log(userTitle);
+console.log(content);
+
+console.log(userInput);
+
+userPostInfo.push({user:userInput, title:userTitle, post:content});
+
+storePost();
+
+console.log(userPostInfo);
+
+});
